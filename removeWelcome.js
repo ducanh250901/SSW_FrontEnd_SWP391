b8ac9246247cf6438502bd6e58396debb0300fcd
Duc Anh <anhndse151522@fpt.edu.vn>
2023-11-25 13:57:25 +0700

    sua giao dien cho logout

--- removeWelcome.js
+++ removeWelcome.js
@@ -1,12 +1,12 @@
 window.addEventListener("load", function () {
-    // var token = localStorage.removeItem("token");
-    // var userName = localStorage.removeItem("userName");
-    // var loggedInUser = document.getElementById("loggedInUser");
+    var token = localStorage.removeItem("token");
+    var userName = localStorage.removeItem("userName");
+    var loggedInUser = document.getElementById("loggedInUser");
 
-    // if (token && userName) {
-    //     // Hiển thị tên người dùng nếu có token
-    //     loggedInUser.innerText = "Welcome " + userName;
-    // }
+    if (token && userName) {
+        // Hiển thị tên người dùng nếu có token
+        loggedInUser.innerText = "Welcome " + userName;
+    }
 
-    this.localStorage.clear();
+    // this.localStorage.clear();
 });
